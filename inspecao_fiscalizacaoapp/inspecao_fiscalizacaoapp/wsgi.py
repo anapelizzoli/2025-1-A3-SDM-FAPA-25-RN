588f62e59c447aa371e22897b5fad959fae47c07
anapelizzoli <sofiapelizzoli22@gmail.com>
2025-06-24 12:08:46 -0300

    criação inicial do projeto inspecao e fiscalização app com o .gitignore

---
+++ inspecao_fiscalizacaoapp/inspecao_fiscalizacaoapp/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for inspecao_fiscalizacaoapp project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inspecao_fiscalizacaoapp.settings')
+
+application = get_wsgi_application()
