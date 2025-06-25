9dce2b8d35bfbcb60f320c804064c80e73275f98
anapelizzoli <sofiapelizzoli22@gmail.com>
2025-06-25 19:47:27 -0300

    adicionando dir apps nas settings

--- inspecao_fiscalizacaoapp/inspecao_fiscalizacaoapp/settings.py
+++ inspecao_fiscalizacaoapp/inspecao_fiscalizacaoapp/settings.py
@@ -12,10 +12,13 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
+import sys
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+APPS_DIR = os.path.join(BASE_DIR, 'apps') 
+sys.path.insert(0, APPS_DIR) 
 
 
 # Quick-start development settings - unsuitable for production
@@ -39,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
