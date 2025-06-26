0e3f7ca9ec945cf838c17423742e51dbb5454ce9
anapelizzoli <sofiapelizzoli22@gmail.com>
2025-06-25 21:11:09 -0300

    Criar o modelo Usuario

--- inspecao_fiscalizacaoapp/apps/usuario/models.py
+++ inspecao_fiscalizacaoapp/apps/usuario/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    nome = models.CharField(max_length=255)
+    email=models.EmailField(unique=True)
+    senha = models.CharField(max_length=255)
+    tipo = models.CharField(max_length=50)
+
